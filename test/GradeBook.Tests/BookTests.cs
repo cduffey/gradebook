7cc293004af036d768a521bee96ead58728334ad
CLIFF DUFFEY <cliff@cliffs-mbp.lan>
2020-11-15 07:44:48 -0500

    Completed Pass by Ref Module

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,13 +6,14 @@ namespace GradeBook.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAverageGrade()
         {
+            //arrange
             var book = new Book("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
-
+       
             var result = book.GetStatistics();
 
             Assert.Equal(85.6, result.Average, 1);
