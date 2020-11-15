ee74f6ef00462ed702365f64d4fe0461a6082128
CLIFF DUFFEY <cliff@cliffs-mbp.lan>
2020-11-15 08:28:22 -0500

    finished control flow

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,42 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Cliffs Gradebook");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while(true) {
+                System.Console.WriteLine("Enter grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if(input == "q") 
+                {
+                    break;
+                }
+
+                try 
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex) 
+                {
+                    System.Console.WriteLine($"{ex.Message}");
+                }
+                catch(FormatException ex)
+                {
+                    System.Console.WriteLine($"{ex.Message}");
+                }
+                finally
+                {
+                    System.Console.WriteLine("**");
+                }
+            }
+
+            
 
             var stats = book.GetStatistics();
             System.Console.WriteLine($"Average grade is {stats.Average:N1}.");
             System.Console.WriteLine($"High grade is {stats.High:N1}.");
             System.Console.WriteLine($"Low grade is {stats.Low:N1}.");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}.");
         }
     }
 }
